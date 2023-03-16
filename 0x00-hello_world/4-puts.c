bd7a5264ef3b5330c13416bd63610f5576af70f5
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-16 18:14:55 +0100

    Hmmm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+  * main - prints the string using puts
+  * Return: Always return 0
+ */
+
+int main(void)
+{
+	char string1[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(string1);
+	return (0);
+}
