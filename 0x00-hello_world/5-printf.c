19d568eb0515b1f089f64bf29bb011942e587765
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-16 18:20:40 +0100

    Hmmm

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+  * main - prints a line
+  * Return: always returns 0
+  */
+
+int main(void)
+{
+	char string1[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", string1);
+	return (0);
+}
