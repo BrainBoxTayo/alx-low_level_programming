b232be9455ba6d7aba6ada1094b3f0f6b963cee7
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-17 10:34:08 +0100

    Hmmm

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+  * main - prints whether the number stored is +ve / -ve
+  * Return: returns 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d ", n);
+	if (n > 0)
+	{
+		printf("is positve\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is negative\n");
+	}
+
+	return (0);
+}
