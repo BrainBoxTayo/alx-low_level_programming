4910d8baa7874b66cfe04d05a9c88cdd0b91b9e8
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-17 16:04:53 +0100

    Putting feature for Capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+  * main - prints alphabet in lower case and upper case
+  * Return: returns 0
+  */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
