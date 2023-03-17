4914786fd254f134708793b90f57e4975d128996
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-17 17:01:21 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+  * print - this function prints the digits from main
+  * @i: storer for the digits in output
+  *
+  * Return: no return from void function
+  */
+void print(int i)
+{
+	putchar(i + '0');
+	if (i == 9)
+	{
+		putchar('\n');
+
+	}
+	else
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	
+}
+/**
+  * main - prints all combinations of single digit numbers of base 10
+  * Return: returns 0 not 1
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print(i);
+	}
+	return (0);
+}
