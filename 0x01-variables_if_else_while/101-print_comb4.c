df4cf3419886b3a4cc6a98787a1b07f10619d135
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-17 20:29:36 +0100

    Wow this took a while but finally solved this using arrays

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+/**
+  * main - prints all combinations of single digit numbers of base 10
+  * Return: returns 0 not 1
+  */
+int main(void)
+{
+	int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int index_storer = 0, index_max = 10;
+	int i, j, k, a, b;
+
+	do {
+	for (a = index_storer; a < index_max; a++)
+	{
+		i = array[index_storer];
+		j = array[a];
+		for (b = index_storer; b < index_max; b++)
+		{
+			k = array[b];
+			if (i == k || i == j || j == k)
+			{
+				continue;
+			}
+			else
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	index_storer++;
+	if (i == 7 && j == 8 && k == 9)
+	{
+		putchar('\n');
+		index_storer = 0;
+	}
+	} while (index_storer);
+	return (0);
+}
