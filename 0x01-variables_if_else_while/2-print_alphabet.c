30936039035268ba9ad37cbd04b15127ee95d775
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-17 15:59:25 +0100

    First commit from challenge 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+  * main - prints alphabet in lower case
+  * Return: returns 0
+  */
+int main(void)
+{
+	char a;
+
+	for(a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
