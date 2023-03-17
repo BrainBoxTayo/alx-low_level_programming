9dcfd6fd47685e19d4859b87d77ac33464e94846
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-17 16:19:09 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+  * main - prints all single digit numbers of base 10
+  * Return: returns 1
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (1);
+}
