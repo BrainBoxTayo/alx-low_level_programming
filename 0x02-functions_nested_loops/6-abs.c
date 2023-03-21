535315a19e828598a1cdf4d9dd2bfc6f3b2f7281
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-21 21:18:50 +0100

    test

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+  * _abs: this is used to get the absolute value of the parameter
+  * @n: stores the value to be made absolute
+  * Return: returns the absolute value of the integer
+  */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
