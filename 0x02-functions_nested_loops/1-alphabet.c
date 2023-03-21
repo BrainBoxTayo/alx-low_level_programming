57e895dc44c079b08dbccbdb30f47fcbe9de6849
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-21 20:06:38 +0100

    test

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include"main.h"
 
 /**
-  * print_alphabet- function to print out the alphabet 
-  *
-  * Return: no return value (void)
-  */
+ * print_alphabet- function to print out the alphabet
+ * Return: no return value (void)
+ */
 
 void print_alphabet(void)
 {
