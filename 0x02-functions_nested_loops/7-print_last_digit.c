5b668a2cf21fe33414b685d3399170d48dc652bc
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-21 21:26:38 +0100

    test

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+  * print_last_digit- prints the last digit of a number
+  * @n: used to store the value of the digit
+  * Return: returns the last digit
+  */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
