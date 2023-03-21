5d4ced9a90e122cc40cf4895ed74224b03fb6157
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-21 22:06:40 +0100

    test

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+  * add- adds two values and returns the sum
+  * @num1: first value
+  * @num2: second value
+  * Return: sum of values
+  */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
