cce7f69f297e47398d8393090c3e5c9928590460
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-22 21:32:56 +0100

    debugs for 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include"main.h"
+
 /**
-  * main - tests functions that check if an integer is positive or negative
+  * main - tests function that prints if integer is positive or negative
   * Return: 0
   */
 
