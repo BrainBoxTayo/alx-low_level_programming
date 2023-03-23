6260e927be58f45b12862b0c9f5e1a29f32ee57c
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-23 18:09:10 +0100

    checks for uppercase characters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
   * _isupper - checks for uppercase characters
-  *
+  * @c: stores the value to be checked
   * Return: 1 if true 0 otherwise
   */
 
 int _isupper(int c)
 {
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
