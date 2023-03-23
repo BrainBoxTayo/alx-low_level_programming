0812cb0a7a4230bceef1d4f147b8ae45a236052b
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-23 18:21:21 +0100

    checks if its a digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+  * mul - multiplies two integers
+  * @a: first integer
+  *
+  * @b: second integer
+  *
+  * Return: multi
+ */
+
+int mul(int a, int b)
+{
+	int multi;
+
+	multi  = (a * b);
+	return (multi);
+}
