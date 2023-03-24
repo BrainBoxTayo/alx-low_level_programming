a857cbc3eb0ca7bfa09a54ed63c0da4a9b613e30
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-24 16:36:56 +0100

    times tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include"main.h"
+/**
+  * times_table - prints the tables of multiplication from 0 - 9
+  *
+  * Return: nothing
+  */
+
+void times_table(void)
+{
+	int i, j, multi;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			multi = i * j;
+			if (multi > 9)
+			{
+				_putchar(multi / 10 + '0');
+				_putchar(multi % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(multi + '0');
+			}
+			if(j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (j == 9)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
