87060a6b1db1332a6359c8049b848f3db4ddb717
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-27 19:36:05 +0100

    reset n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"main.h"
+/**
+  * reset_to_98 - takes a pointer and updates its value to 98
+  * @n: pointer value
+  * Return: nothing
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
