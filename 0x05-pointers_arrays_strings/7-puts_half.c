2af8dafb36213c387908355d1d03ef5c4a55d425
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-27 21:22:29 +0100

    this puts every other character

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include"main.h"
+
+/**
+  * puts_half - prints half of the string
+  * @str: string to be printed
+  * Return: nothing
+  */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int size;
+	/*gets the size without strlen*/
+	for (size = 0; str[size] != '\0'; size++)
+		;
+	/*this gets half of the string for even lengths*/
+	if (size % 2 == 0)
+	{
+		for (i = size / 2; i <= size; i++)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	/*this gets half of the string for odd lengths*/
+	else
+	{
+		for (i = ((size - 1) / 2); i <= size; i++)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	_putchar('\n');
+}
