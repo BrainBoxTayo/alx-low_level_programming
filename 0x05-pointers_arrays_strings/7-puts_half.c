9101032677921e90204e689a563082e1efc8ab0b
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-27 21:40:30 +0100

    this puts every other character

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,12 @@ void puts_half(char *str)
 	int i = 0;
 	int size;
 	/*gets the size without strlen*/
-	for (size = 0; str[size] != '\0'; size++)
+	for (size = 0; *(str + size) != '\0'; ++size)
 		;
 	/*this gets half of the string for even lengths*/
 	if (size % 2 == 0)
 	{
-		for (i = size / 2; i < size; i++)
+		for (i = size / 2; str[i] != '\0'; ++i)
 		{
 			_putchar(*(str + i));
 		}
@@ -24,7 +24,7 @@ void puts_half(char *str)
 	/*this gets half of the string for odd lengths*/
 	else
 	{
-		for (i = ((size - 1) / 2); i < size; i++)
+		for (i = ((size - 1) / 2) + 1; str[i] != '\0'; ++i)
 		{
 			_putchar(*(str + i));
 		}
