8bc08d02a9875e72a1dbe1f7da8f5d8e14a70892
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-29 19:19:26 +0100

    strncat amateur try

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,14 +3,14 @@
   * _strncpy - copies a string from src into dest using n bytes of src
   * @dest: destination string
   * @src: source string
-  * n: number of bytes
+  * @n: number of bytes
   * Return: pointer to dest
   */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n && *(dest + i) != '\0'; i++)
 	{
 		dest[i] = src[i];
