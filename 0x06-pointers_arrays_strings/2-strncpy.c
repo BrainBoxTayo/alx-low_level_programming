38267d3c605b17898c2ce53867faa65ccad08da4
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-29 19:12:48 +0100

    strncat amateur try

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+  * _strncpy - copies a string from src into dest using n bytes of src
+  * @dest: destination string
+  * @src: source string
+  * n: number of bytes
+  * Return: pointer to dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+	for (i = 0; i < n && *(dest + i) != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
