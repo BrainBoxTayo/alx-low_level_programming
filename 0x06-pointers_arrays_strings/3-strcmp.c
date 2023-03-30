81bbbb65e0069f15edce5db07dbc6fff8eedcd6b
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-03-30 18:00:58 +0100

    fixed the strcmp function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include"main.h"
+#define DC2 18;
 /**
   * _strcmp - compares two strings
   * @s1: first string
@@ -20,16 +21,16 @@ int _strcmp(char *s1, char *s2)
 	{
 		for (i = 0; i == size_s1; i++)
 		{
-			s2[size_s2 + i] = '\0';
+			s2[size_s2 + i] = '0';
 		}
 	}
 	if (size_s1 > size_s2)
 	{
-		store = s1[size_s1 - 1] - s2[size_s1 - 1];
+		store = '0' - (s1[size_s1 - 1] - s2[size_s1 - 1]);
 	}
 	else if (size_s1 < size_s2)
 	{
-		store = '\0' - s2[size_s2 - 1];
+		store = s2[size_s2 - 1] - '0';
 	}
 	else
 	{
