cc3cfde76d6010cb08d0fd8fd9b13fecb2584214
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-04 20:17:41 +0100

    *s = to

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
