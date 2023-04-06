6d111a5a91d775879a95e5846a548610497d086b
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-06 18:38:20 +0100

    square root function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include"main.h"
+/**
+  * prime_checker - helper function
+  * @n: number
+  * @ctr: decrement variable
+  * Return: 1 or 0
+  */
+int prime_checker(int n, int ctr)
+{
+	if (ctr == 1)
+	{
+		return (1);
+	}
+	if (n % ctr == 0 && n != ctr && n != 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime_checker(n, ctr - 1));
+	}
+}
+/**
+  * is_prime_number - checks if a number is a prime number or not
+  * @n: number to be checked
+  * Return: 0(is not) or 1(is prime)
+  */
+
+int is_prime_number(int n)
+{
+	if (n == 1 || n < 0)
+		return (0);
+	return (prime_checker(n, n));
+}
