a48d6a3ea70aeba8cb1eb38d122931ed5b007ca3
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-06 18:06:43 +0100

    square root function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,21 +2,22 @@
 /**
   * square_root_helper - helper function for _sqrt_recursion
   * @n: number to be rooted
-  * upper: upper limit
-  * lower: lower limit
+  * @upper: upper limit
+  * @lower: lower limit
   * Return: result
   */
 int square_root_helper(int n, int upper, int lower)
 {
 	int mid, result;
-        if (upper * upper == n)
-                return upper;
-        if (upper == lower)
-                return -1;
-        mid = (upper + lower) / 2;
-        if (mid < n / mid)
-                return square_root_helper(n, upper, mid + 1);
-        result = square_root_helper(n, mid, lower);
+
+	if (upper * upper == n)
+		return (upper);
+	if (upper == lower)
+		return (-1);
+	mid = (upper + lower) / 2;
+	if (mid < n / mid)
+		return (square_root_helper(n, upper, mid + 1));
+	result = square_root_helper(n, mid, lower);
 	if (result * result != n)
 	{
 		return (-1);
@@ -24,7 +25,6 @@ int square_root_helper(int n, int upper, int lower)
 	else
 		return ((result == -1) ? mid : result);
 }
-
 /**
  * _sqrt_recursion - returns the square root
  * @n: number to be rooted
@@ -32,10 +32,10 @@ int square_root_helper(int n, int upper, int lower)
  */
 int _sqrt_recursion(int n)
 {
-        if (n < 0)
-                return -1;
-        if (n == 0)
-                return 0;
-        return square_root_helper(n, n, 0);
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	return (square_root_helper(n, n, 0));
 }
 
