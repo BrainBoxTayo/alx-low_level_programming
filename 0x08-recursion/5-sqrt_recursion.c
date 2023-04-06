7967bed3b55adb1c16f12a450a0a0cd79dd03105
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-06 17:58:15 +0100

    square root function

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include"main.h"
+/**
+  * square_root_helper - helper function for _sqrt_recursion
+  * @n: number to be rooted
+  * upper: upper limit
+  * lower: lower limit
+  * Return: result
+  */
+int square_root_helper(int n, int upper, int lower)
+{
+	int mid, result;
+        if (upper * upper == n)
+                return upper;
+        if (upper == lower)
+                return -1;
+        mid = (upper + lower) / 2;
+        if (mid < n / mid)
+                return square_root_helper(n, upper, mid + 1);
+        result = square_root_helper(n, mid, lower);
+	if (result * result != n)
+	{
+		return (-1);
+	}
+	else
+		return ((result == -1) ? mid : result);
+}
+
+/**
+ * _sqrt_recursion - returns the square root
+ * @n: number to be rooted
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+        if (n < 0)
+                return -1;
+        if (n == 0)
+                return 0;
+        return square_root_helper(n, n, 0);
+}
+
