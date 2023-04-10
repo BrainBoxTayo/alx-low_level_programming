5777c9254de2ae23d9c395267eac8fa2a8cdb522
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-10 11:10:46 +0100

    a bit of fibonnacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,26 @@
 #include<stdio.h>
+/**
+  * fibo_helper - helper for main
+  * @length: length of the series
+  * Return: nothing
+  */
+void fibo_helper(int length)
+{
+	static unsigned long long int num1 = 0, num2 = 1, num3;
+	if (length > 0)
+	{
+		num3 = num1 + num2;
+		num1 = num2;
+		num2 = num3;
+		printf(" %llu,", num3);
+		fibo_helper(length - 1);
+	}
+	else
+	{
+		return;
+	}
+}
+
 
 /**
   * main - prints the fibonacci series to 50
@@ -7,19 +29,10 @@
 
 int main(void)
 {
-	unsigned long int fibo_storer[50] = {0}, fibo = 1;
-	int i = 1;
+	int num = 0, length = 48;
 
-	
-	do {
-		if (i == 50)
-		{
-			printf("%ld", fibo);
-		}
-		printf("%ld, ",fibo);
-		fibo = fibo + fibo_storer[i - 1];
-		fibo_storer[i + 1] = fibo;
-		i++;
-	} while (i <= 50);
+	printf("%d, %d,", num, num + 1);
+	fibo_helper(length);
+	printf("\b \n");
 	return (0);
 }
