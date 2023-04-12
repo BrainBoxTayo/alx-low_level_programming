a2eb19c5db2754ee716857a3648aad11dd77a537
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-12 19:20:02 +0100

    2d array malloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stlib.h>
+/**
+  * free_grid - frees a 2 dimensional grid
+  * @grid: grid from alloc_grid function
+  * @height: height of grid
+  * Return: nothing
+  */
+
+void free_grid(int **grid, int height)
+{
+
+}
