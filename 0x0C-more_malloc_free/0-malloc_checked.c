f4d20d9bfa47564879cb36de98088944904526c3
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-13 18:06:43 +0100

    normal process termination malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include"main.h"
+#define EXIT_ALX 98
+/**
+  * malloc_checked - allocates memory using malloc
+  * @b: size of memory to be allocated
+  * Return: pointer to the allocated memory
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(EXIT_ALX);
+	return ptr;
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+# More Malloc more Free
