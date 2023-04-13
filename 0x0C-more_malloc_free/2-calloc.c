2937e3e1c5fb7f0f6af30b11b6795c6346b9f977
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-13 19:10:21 +0100

    a char/int hiccup

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+  * _calloc - allocates memory for an array of nmemb elements of size bytes
+  * @nmemb: number of elements
+  * @size: number of bytes of type
+  * Return: pointer to the allocated memory.
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr = malloc(nmemb * size);
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		*(ptr + i) = 0;
+	return (ptr);
+}
