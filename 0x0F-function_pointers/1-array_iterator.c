12db365ab72d7afafd36bbfd0de9fd85661cda8d
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-19 20:17:52 +0100

    makes a function act on each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"function_pointers.h"
+/**
+  * array_iterator - executes a function on each element of an array
+  * @array: array
+  * @size: size of the array
+  * @action: pointer to the function
+  * Return: Nothing
+  */
+
+void array_iterator(int *array, size_t size, void(* action)(int))
+{
+	long unsigned int i;
+	for (i = 0; i < size; i++)
+	{
+		if (array && action)
+			action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef FUNC_POINT_H
 #define FUNC_POINT_H
+
+void array_iterator(int *array, size_t size, void(* action)(int));
 void print_name(char *name, void (*f)(char *));
 #endif
