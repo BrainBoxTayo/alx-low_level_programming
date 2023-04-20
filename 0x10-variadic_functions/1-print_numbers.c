8eaa58a42caec35ba22c9f56c55eeb450f08a6c9
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-20 23:15:32 +0100

    string printer

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,6 @@ void print_numbers(const char *seperator, const unsigned int n, ...)
 	unsigned int i;
 	va_list intVar;
 
-	if (n == 0)
-		return;
 	va_start(intVar, n);
 	for (i = 1; i < n; i++)
 	{
