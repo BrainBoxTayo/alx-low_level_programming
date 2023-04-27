ed4ae8c5b13e2b6f5ab6f2c02908777069b72be4
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-27 19:52:45 +0100

    newnodes added

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include"lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+  * add_node - adds elements to a list
+  * @head: pointer to a pointer to a list_t list
+  * @str: string to be put into str 
+  * Return: number of nodes
+  */
+
+size_t add_node(list_t **head, const char *str)
+{
+	char *str_n;
+	int length;
+	list_t *newnode;
+
+	newnode = malloc(sizeof(list_t));
+	if(new == NULL)
+		return (NULL);
+	str_n = strdup(str);
+	if (str_n == NULL)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	for (length = 0; str[length]; length++)
+		;
+	newnode->str = str_n;
+	newnode->len = length;
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
+}
