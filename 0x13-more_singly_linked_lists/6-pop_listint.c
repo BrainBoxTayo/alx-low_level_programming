53edbaea99ab8a5c1eaaf64c2e87956c314ca9d6
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-05-01 13:24:15 +0100

    pop operation on a linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include"lists.h"
+/**
+  * pop_listint - deletes the head node of a list
+  * @head: pointer to the head node
+  * Return: head node's data(n)
+  */
+int pop_listint(listint_t **head)
+{
+	int n_store;
+	
+	listint_t *node;
+	if (head == NULL)
+	{
+		return (0);
+	}
+	
+	node = (*head)->next;
+	n_store = (*head)->n;
+	free(*head);
+	(*head) = node;
+	return (n_store);
+}
