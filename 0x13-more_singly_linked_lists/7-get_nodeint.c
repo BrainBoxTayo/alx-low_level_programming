62d2f20dbab05022edf96d88226d2939e0029a9b
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-05-01 13:50:45 +0100

    returns the nth node of a linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,8 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *node;
 	unsigned int count = 0;
-	if (head == NULL)
-		return (NULL);
+	
 	node = head;
 	while (node != NULL)
 	{
