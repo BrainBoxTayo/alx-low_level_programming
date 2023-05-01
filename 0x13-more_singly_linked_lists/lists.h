0dacb7aa8866263619ea434b5a8015151e2a936f
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-05-01 12:00:29 +0100

    more singly linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include<stdio.h>
 #include<stdlib.h>
 /**
@@ -13,7 +12,6 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
 size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h)
+size_t listint_len(const listint_t *h);
 #endif
