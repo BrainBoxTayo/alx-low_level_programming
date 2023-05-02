073d0c3edcef5cb82892c8a8e8183da04b21d525
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-05-02 18:24:23 +0100

    attribute constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include"lists.h"
+/**
+  * construct - prints before main function
+  * Return: Nothing
+  */
+void construct(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
