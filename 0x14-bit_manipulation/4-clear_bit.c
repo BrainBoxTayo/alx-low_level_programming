b8d60fe38846ebef1998ac4d74b7330e103169e7
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-05-04 20:29:53 +0100

    clears the bit at the given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+  * clear_bit - clears the value of a bit at the given index
+  * @n: pointer to a number
+  * @index: index we are lookin gat
+  * Return: 1 or -1(error)
+  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+	*n = *n & ~(1 << index);
+	return (1);
+}
