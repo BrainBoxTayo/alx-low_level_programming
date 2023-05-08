b2507c307bdd3c973f6518214ddbb6c4307f520e
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-05-08 20:57:24 +0100

    open, read and write used

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include<unistd.h>
+#include<fcntl.h>
+#include"main.h"
+/**
+  * read_textfile - reads a text file and prints it to the POSIX standard
+  * output
+  * @filename: name of file
+  * @letters: number of letters to read and print
+  * Return: actual number of letters it could read and print
+  * else 0 if it cannot opened or filename is NULL
+  * or write fails
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, lett_read = 0;
+	char *c = malloc(letters * sizeof(char));
+
+	if (filename == NULL || c == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	lett_read = read(fd, c, letters);
+	write(STDIN_FILENO, c, lett_read);
+	return (lett_read);
+}
