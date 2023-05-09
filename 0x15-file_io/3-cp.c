4cfddaa6d4664ea23d4e4eb6c10c14729e1c6b00
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-05-09 20:08:18 +0100

    cp function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,29 +9,55 @@ void cp(char *file_from, char *file_to)
 {
 	int fd, fd2;
 	int closeflag;
+	int rflag = 1;
+	int wflag = 1;
+	char *buffer;
 
+	buffer = malloc(sizeof(char) * BUFFSIZE);
+	if (buffer == NULL)
+		exit(101);
 	fd = open(file_from, O_RDONLY);
 	if (fd < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", file_from);
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	fd2 = open(file_to, O_WRONLY, 0664);
+	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (fd2 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", file_to);
+		dprintf(2, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
-	closeflag = close(fd);
-	if (closeflag < 0)
+	while (rflag)
 	{
-		 dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
+		rflag = read(fd, buffer, BUFFSIZE);
+		if (rflag < 0)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", file_from);
+			exit(99);
+		}
+		if (rflag == 0)
+			break;
+		wflag = write(fd2, buffer, rflag);
+		if (wflag < 0)
+		{
+			dprintf(2, "Error: Can't write to file %s\n", file_to);
+			exit(99);
+		}
 	}
 	closeflag = close(fd2);
 	if (closeflag < 0)
 	{
-		 dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2);
+		dprintf(2, "Error: Can't close fd %d\n", fd2);
+		exit(100);
 	}
+	closeflag = close(fd);
+	if (closeflag < 0)
+	{
+		 dprintf(2, "Error: Can't close fd %d\n", fd);
+		 exit(100);
+	}
+	free(buffer);
 }
 /**
   * main - execution of cp
@@ -43,8 +69,9 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
+	cp(argv[1], argv[2]);
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include<stdlib.h>
+#include<stdio.h>
 #include<unistd.h>
 #include<fcntl.h>
+#include<stdlib.h>
 #include<stddef.h>
+#define BUFFSIZE 1024
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -27,4 +30,5 @@ char *_strstr(char *haystack, char *needle);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void cp(char *file_from, char *file_to);
 #endif
