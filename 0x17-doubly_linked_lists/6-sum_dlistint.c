0e1ccd2c69951c33dd780bde92bc2cc641a01687
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-15 19:37:57 +0100

    sum od all data in a linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include"lists.h"
+/**
+  * sum_dlistint - returns sum of elements in a linked list
+  * @h: pointer to head of linked list
+  * Return: Number of elements
+  */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+	int i = 0;
+
+	node = head;
+	while (node != NULL)
+	{
+		i += node->n;
+		node = node->next;
+	}
+	return (i);
+}
