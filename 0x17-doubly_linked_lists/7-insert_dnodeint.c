54f65b0491c8f32e32f9847435b5b962cc0450d4
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-15 20:14:56 +0100

    working

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -40,14 +40,14 @@ size_t dlistint_len(const dlistint_t *h)
 /**
   * insert_dnodeint_at_index -  inserts a new node at a given position.
   * @h: pointer to a pointer to the header of first node
-  * idx: index
-  * n: n value
+  * @idx: index
+  * @n: n value
   * Return: the address of the new node, or NULL if it failed
   */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *node, *nodeA;
-	
+
 	if (h == NULL)
 		return (NULL);
 	nodeA = malloc(sizeof(dlistint_t));
