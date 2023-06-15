e722240744436b8c1a0c5a314e1f30d31761043c
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-15 18:54:07 +0100

    adds to the bottom of the list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,34 @@
 #include"lists.h"
 /**
   * add_dnodeint_end - adds a node at the end of a linked list
-  * @head: po
+  * @head: pointer to head
+  * @n: data stored
+  * Return: pointer to a linked list node
   */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *nodeT = *head;
+	dlistint_t *node = malloc(sizeof(dlistint_t));
+
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = NULL;
+	if (nodeT == NULL)
+	{
+		node->prev = nodeT;
+		*head = node;
+		return (node);
+	}
+	while (nodeT != NULL)
+	{
+		if (nodeT->next == NULL)
+		{
+			node->prev = nodeT;
+			(nodeT)->next = node;
+			break;
+		}
+		nodeT = nodeT->next;
+	}
+	return (node);
+}
