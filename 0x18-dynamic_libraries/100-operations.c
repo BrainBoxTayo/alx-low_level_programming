31736ffb709db614261e34cc88948160de3cca76
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-06-19 20:24:27 +0100

    100-operations.so working in a python program

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include"ops.h"
+/**
+  * add - adds two ints together
+  * @a: first int
+  * @b: second int
+  * Return: added value
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * sub - subtracts two ints
+  * @a: first int
+  * @b: second int
+  * Return: subbed value
+  */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * mul - multiplies two ints together
+  * @a: first int
+  * @b: second int
+  * Return: multiplied value
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * div - divides two ints together
+  * @a: first int
+  * @b: second int
+  * Return: divided value
+  */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * mod - remainder of two ints
+  * @a: first int
+  * @b: second int
+  * Return: remainder value
+  */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/ops.h
@@ -0,0 +1,9 @@
+#ifndef OPS_H
+#define OPS_H
+
+int div(int a, int b);
+int mod(int a, int b);
+int mul(int a, int b);
+int sub(int a, int b);
+int add(int a, int b);
+#endif
