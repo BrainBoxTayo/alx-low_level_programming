6827728a6d88823de476dced39597218295e2dfd
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-06 19:33:11 +0100

    hash tables creation

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+  * hash_table_create - creates a hash table
+  * @size: size of the array
+  * Return: Pointer to the newly created hash table
+  * if something goes wrong return NULL
+  */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash = malloc(sizeof(hash_table_t));
+	if (new_hash == NULL)
+		return (NULL);
+	new_hash->size = size;
+	new_hash->array = NULL;
+	return (new_hash);
+}
