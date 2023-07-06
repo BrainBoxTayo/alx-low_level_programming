f4b028c0745437e704aa56d08b7dd94f0d068af3
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-06 19:33:56 +0100

    hash tables creation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash = malloc(sizeof(hash_table_t));
+
 	if (new_hash == NULL)
 		return (NULL);
 	new_hash->size = size;
