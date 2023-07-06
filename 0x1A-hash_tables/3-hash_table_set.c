cc819664a6d55d373400db5422668252fef9c2b2
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-06 20:55:59 +0100

    fixing some errors concerning stuff passed to the function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node, *collision_node;
 	unsigned long int index;
 
-	if (key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
