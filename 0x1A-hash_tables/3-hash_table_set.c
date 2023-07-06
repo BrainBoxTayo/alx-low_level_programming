b71286ebca175427f5d6eae3970e518dc8f23aab
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-07-06 21:45:18 +0100

    hash_table_get function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,14 +11,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node, *collision_node;
 	unsigned long int index;
 
-	if (key == NULL || ht == NULL || strlen(key) == 0)
+	if (key == NULL || ht == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->value = strdup(value);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new_node;
